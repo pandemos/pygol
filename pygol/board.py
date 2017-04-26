34066ebbd4eb6102965db3c3c72177e57230d54a
Allison Knauss <allison.knauss@nerdery.com>
2017-04-26 13:59:25 -0500

    Tests and implementation of Board

--- pygol/board.py
+++ pygol/board.py
@@ -2,14 +2,90 @@ import cell
 
 class Board(object):
 
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width = 0, height = 0, iteration=0):
         self.width = width
         self.height = height
+
+        # Good example of this approach working:
+        # I had initially forgotten to change this from `= 0` to `= iteration`
+        # Without the specific test, even though this is merely an assignment,
+        # that might have survived much longer.
+        self.iteration = iteration
+
         self.cells = [[cell.DeadCell() for x in range(width)] for y in range(height)]
     
     def setCellAt(self, x, y, cell):
+
+        # We don't observe the magical Python indexing here. x and y must be absolute
+        # indices into the board.
+        if y < 0 or y >= self.height or x < 0 or x >= self.width:
+            raise IndexError("Attempt to set cell at invalid position {0},{1}", [x, y])
+
         self.cells[y][x] = cell
 
     def cellAt(self, x, y):
+
+        # We don't observe the magical Python indexing here. x and y must be absolute
+        # indices into the board.
+        if y < 0 or y >= self.height or x < 0 or x >= self.width:
+            raise IndexError("Attempt to get cell at invalid position {0},{1}", [x, y])
+
         return self.cells[y][x]
+
+    def step(self):
+        self.iteration += 1
+
+    def reset(self):
+        self.iteration = 0
+
+    def livingNeighbors(self, x, y):
+        neighbors = [
+            self.neighborAt(self.f_filter_above(x, y), self.f_at_above(x, y)),
+            self.neighborAt(self.f_filter_below(x, y), self.f_at_below(x, y)),
+            self.neighborAt(self.f_filter_left(x, y), self.f_at_left(x, y)),
+            self.neighborAt(self.f_filter_right(x, y), self.f_at_right(x, y)),
+            self.neighborAt(self.f_filter_UL(x, y), self.f_at_UL(x, y)),
+            self.neighborAt(self.f_filter_UR(x, y), self.f_at_UR(x, y)),
+            self.neighborAt(self.f_filter_LL(x, y), self.f_at_LL(x, y)),
+            self.neighborAt(self.f_filter_LR(x, y), self.f_at_LR(x, y)),
+        ]
+        return sum([1 for neighbor in neighbors if neighbor == cell.Alive])
+
+    def f_filter_above(self, x, y):
+        return lambda: y == 0
+    def f_at_above(self, x, y): 
+        return lambda: self.cellAt(x, y-1)
+    def f_filter_below(self, x, y):
+        return lambda: y == self.height-1
+    def f_at_below(self, x, y): 
+        return lambda: self.cellAt(x, y+1)
+    def f_filter_left(self, x, y):
+        return lambda: x == 0
+    def f_at_left(self, x, y): 
+        return lambda: self.cellAt(x-1, y)
+    def f_filter_right(self, x, y):
+        return lambda: x == self.width-1
+    def f_at_right(self, x, y): 
+        return lambda: self.cellAt(x+1, y)
+    def f_filter_UL(self, x, y):
+        return lambda: x == 0 or y == 0
+    def f_at_UL(self, x, y): 
+        return lambda: self.cellAt(x-1, y-1)
+    def f_filter_UR(self, x, y):
+        return lambda: x == self.width-1 or y == 0
+    def f_at_UR(self, x, y): 
+        return lambda: self.cellAt(x+1, y-1)
+    def f_filter_LL(self, x, y):
+        return lambda: x == 0 or y == self.height-1
+    def f_at_LL(self, x, y): 
+        return lambda: self.cellAt(x-1, y+1)
+    def f_filter_LR(self, x, y):
+        return lambda: x == self.width-1 or y == self.height-1
+    def f_at_LR(self, x, y): 
+        return lambda: self.cellAt(x+1, y+1)
+
+    def neighborAt(self, f_filter, f_at):
+        if f_filter():
+            return cell.Dead
+        return f_at().mortality
 
--- test/pygol/test_board.py
+++ test/pygol/test_board.py
@@ -6,19 +6,145 @@ import unittest
 class TestBoard(unittest.TestCase):
 
     def test_has_width_and_height(self):
-        b = board.Board(width = 10, height = 10)
+        """ A board is primarily specified by its width and height """
+
+        b = board.Board(width=10, height=10)
         self.assertEquals(b.width, 10)
         self.assertEquals(b.height, 10)
 
     def test_can_retrieve_cell_by_coordinate(self):
+        """ Any cell can be retrieved from the board by specifying an x and y coordinate """
+
         c = cell.LiveCell()
-        b = board.Board(width = 10, height = 10)
+        b = board.Board(width=10, height=10)
 
         # Living cell
-        b.setCellAt(x = 3, y = 3, cell = c)
+        b.setCellAt(x=3, y=3, cell=c)
         self.assertEquals(b.cellAt(3, 3), c)
 
         # Dead cell
         self.assertEquals(b.cellAt(7, 7).mortality, cell.Dead)
 
+    def test_iteration_count_starts_at_zero(self):
+        """ Iteration count starts at 0 """
+        b = board.Board(width=10, height=10)
+        self.assertEquals(b.iteration, 0)
+
+    def test_has_iteration_count(self):
+        """ Board has an iteration count """
+        
+        b = board.Board(width=10, height=10, iteration=4)
+        self.assertEquals(b.iteration, 4)
+
+    def test_iteration_count_can_be_incremented(self):
+        """ Iteration count can be incremented """
+
+        b = board.Board(width=10, height=10)
+        self.assertEquals(b.iteration, 0)
+        b.step()
+        self.assertEquals(b.iteration, 1)
+        b.step()
+        self.assertEquals(b.iteration, 2)
+
+    def test_iteration_count_can_be_reset(self):
+        """ Iteration count can be reset """
+
+        b = board.Board(width=10, height=10, iteration=4)
+        b.reset()
+        self.assertEquals(b.iteration, 0)
+
+    def test_out_of_range_cell(self):
+        """ Attempt to access a cell outside the board throws an exception """
+
+        b = board.Board(width=10, height=10)
+        
+        with self.assertRaises(IndexError):
+            b.setCellAt(-1, -1, cell.LiveCell())
+        with self.assertRaises(IndexError):
+            b.cellAt(-1, -1)
+        with self.assertRaises(IndexError):
+            b.cellAt(b.width, b.height)
+
+    def test_determine_number_of_living_zero_neighbors(self):
+        """ For any cell, determine the number of living neighbors when no neighbors are living """
+
+        b = board.Board(width=3, height=3)
+        b.setCellAt(1, 1, cell.LiveCell())
+        self.assertEquals(b.livingNeighbors(1, 1), 0)
+
+    def test_determine_number_of_living_one_neighbor_edges(self):
+        """ For any cell, determine the number of living neighbors when one edge neighbor is living """
+
+        b = board.Board(width=3, height=3)
+        b.setCellAt(1, 1, cell.LiveCell())
+        b.setCellAt(1, 0, cell.LiveCell())
+        self.assertEquals(b.livingNeighbors(1, 1), 1)
+
+        b = board.Board(width=3, height=3)
+        b.setCellAt(1, 1, cell.LiveCell())
+        b.setCellAt(0, 1, cell.LiveCell())
+        self.assertEquals(b.livingNeighbors(1, 1), 1)
+
+        b = board.Board(width=3, height=3)
+        b.setCellAt(1, 1, cell.LiveCell())
+        b.setCellAt(2, 1, cell.LiveCell())
+        self.assertEquals(b.livingNeighbors(1, 1), 1)
+
+        b = board.Board(width=3, height=3)
+        b.setCellAt(1, 1, cell.LiveCell())
+        b.setCellAt(1, 2, cell.LiveCell())
+        self.assertEquals(b.livingNeighbors(1, 1), 1)
+
+    def test_determine_number_of_living_two_neighbor_edges(self):
+        """ For any cell, determine the number of living neighbors when one edge neighbor is living """
+
+        b = board.Board(width=3, height=3)
+        b.setCellAt(1, 1, cell.LiveCell())
+        b.setCellAt(1, 0, cell.LiveCell())
+        b.setCellAt(0, 1, cell.LiveCell())
+        self.assertEquals(b.livingNeighbors(1, 1), 2)
+
+        b = board.Board(width=3, height=3)
+        b.setCellAt(1, 1, cell.LiveCell())
+        b.setCellAt(2, 1, cell.LiveCell())
+        b.setCellAt(1, 2, cell.LiveCell())
+        self.assertEquals(b.livingNeighbors(1, 1), 2)
+
+    def test_determine_number_of_living_one_neighbor_corner(self):
+        """ For any cell, determine the number of living neighbors when one corner neighbor is living """
+
+        b = board.Board(width=3, height=3)
+        b.setCellAt(1, 1, cell.LiveCell())
+        b.setCellAt(0, 0, cell.LiveCell())
+        self.assertEquals(b.livingNeighbors(1, 1), 1)
+        
+        b = board.Board(width=3, height=3)
+        b.setCellAt(1, 1, cell.LiveCell())
+        b.setCellAt(0, 2, cell.LiveCell())
+        self.assertEquals(b.livingNeighbors(1, 1), 1)
+        
+        b = board.Board(width=3, height=3)
+        b.setCellAt(1, 1, cell.LiveCell())
+        b.setCellAt(2, 0, cell.LiveCell())
+        self.assertEquals(b.livingNeighbors(1, 1), 1)
+        
+        b = board.Board(width=3, height=3)
+        b.setCellAt(1, 1, cell.LiveCell())
+        b.setCellAt(2, 2, cell.LiveCell())
+        self.assertEquals(b.livingNeighbors(1, 1), 1)
+        
+    def test_determine_number_of_living_two_neighbor_corner(self):
+        """ For any cell, dtermine the number of living neighbors when two corner neighbors are living """
+
+        b = board.Board(width=3, height=3)
+        b.setCellAt(1, 1, cell.LiveCell())
+        b.setCellAt(0, 0, cell.LiveCell())
+        b.setCellAt(2, 2, cell.LiveCell())
+        self.assertEquals(b.livingNeighbors(1, 1), 2)
+
+        b = board.Board(width=3, height=3)
+        b.setCellAt(1, 1, cell.LiveCell())
+        b.setCellAt(2, 0, cell.LiveCell())
+        b.setCellAt(0, 2, cell.LiveCell())
+        self.assertEquals(b.livingNeighbors(1, 1), 2)
 
