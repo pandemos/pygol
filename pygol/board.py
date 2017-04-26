73c1b4bb1729bdcd1beeff36b5a16dfcf8affd63
Allison Knauss <allison.knauss@nerdery.com>
2017-04-26 11:43:28 -0500

    Implement set/get cell on board

--- pygol/board.py
+++ pygol/board.py
@@ -1,5 +1,15 @@
+import cell
+
 class Board(object):
 
     def __init__(self, width = 0, height = 0):
         self.width = width
         self.height = height
+        self.cells = [[cell.DeadCell() for x in range(width)] for y in range(height)]
+    
+    def setCellAt(self, x, y, cell):
+        self.cells[y][x] = cell
+
+    def cellAt(self, x, y):
+        return self.cells[y][x]
+
--- test/pygol/test_board.py
+++ test/pygol/test_board.py
@@ -1,4 +1,5 @@
 from pygol import board
+from pygol import cell
 
 import unittest
 
@@ -8,4 +9,16 @@ class TestBoard(unittest.TestCase):
         b = board.Board(width = 10, height = 10)
         self.assertEquals(b.width, 10)
         self.assertEquals(b.height, 10)
+
+    def test_can_retrieve_cell_by_coordinate(self):
+        c = cell.LiveCell()
+        b = board.Board(width = 10, height = 10)
+
+        # Living cell
+        b.setCellAt(x = 3, y = 3, cell = c)
+        self.assertEquals(b.cellAt(3, 3), c)
+
+        # Dead cell
+        self.assertEquals(b.cellAt(7, 7).mortality, cell.Dead)
+
 
